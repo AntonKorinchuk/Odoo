a5d64a2e6924ef375bcd2218745120ab555651bb
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-09-27 20:32:24 +0300

    Add manifest

---
+++ custom_addons/persons_module/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Persons Module",
+    "version": "1.0",
+    "depends": ["base", "website"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/persons_views.xml",
+        "views/persons_template.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
